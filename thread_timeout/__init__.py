8ba43b8907fa96427fffaefaf341bd39611bb166
followcat <followcat@163.com>
2016-07-28 14:58:17 +0800

    Modify GPL version from 2.1 to 3.

--- thread_timeout/__init__.py
+++ thread_timeout/__init__.py
@@ -4,7 +4,7 @@
 # This library is free software; you can redistribute it and/or
 # Modify it under the terms of the GNU Lesser General Public
 # License as published by the Free Software Foundation; either
-# Version 2.1 of the License, or (at your option) any later version.
+# Version 3 of the License, or (at your option) any later version.
 #
 # This library is distributed in the hope that it will be useful,
 # But WITHOUT ANY WARRANTY; without even the implied warranty of
